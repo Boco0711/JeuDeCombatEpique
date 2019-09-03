bbf73aa4575837d1c53d834bcca260d7f95edd09
Boco0711 <s.lep.0711@gmail.com>
2019-09-03 13:10:42 +0200

    Création des 2 personnages

--- src/com/ocr/sylvain/Main.java
+++ src/com/ocr/sylvain/Main.java
@@ -3,6 +3,7 @@ package com.ocr.sylvain;
 public class Main {
 
     public static void main(String[] args) {
-        Character Joueur1 = new Character();
+        Character joueur1 = new Character("Joueur 1");
+        Character joueur2 = new Character("Joueur 2");
     }
 }
