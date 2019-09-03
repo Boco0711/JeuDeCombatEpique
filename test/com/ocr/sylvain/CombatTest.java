0c25dff28e97ba27ecbb4a28c7a0c0978cfb64dd
Boco0711 <s.lep.0711@gmail.com>
2019-09-03 13:12:59 +0200

    Création de la Classe Combat et sa classeTest

---
+++ test/com/ocr/sylvain/CombatTest.java
@@ -0,0 +1,25 @@
+package com.ocr.sylvain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CombatTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(System.out);
+    }
+
+}
