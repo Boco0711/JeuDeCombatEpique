8f9140f842d8650dac5dde49fed80f7adfd58a51
Boco0711 <s.lep.0711@gmail.com>
2019-09-05 00:42:08 +0200

    Modification des test CombatTest

--- test/com/ocr/sylvain/CombatTest.java
+++ test/com/ocr/sylvain/CombatTest.java
@@ -33,11 +33,18 @@ class CombatTest {
 
     @Test
     public void Given_WarriorBasicAttack_When_CombatIsRun_Then_DisplaySentence() {
-        System.setIn(new ByteArrayInputStream("1\n".getBytes()));
+        System.setIn(new ByteArrayInputStream("1\n1\n1\n1\n1\n1\n1\n1\n1\n".getBytes()));
         Combat combat = new Combat(joueur1, joueur2);
         combat.startCombat(joueur1, joueur2);
         String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
         assertEquals("Joueur 1 utilise Coup d'Épée et inflige 100 dommages", output[1]);
+        assertEquals("Joueur 2 utilise Coup d'Épée et inflige 100 dommages", output[]);
+        assertEquals("Joueur 1 utilise Coup d'Épée et inflige 100 dommages", output[1]);
+        assertEquals("Joueur 2 utilise Coup d'Épée et inflige 100 dommages", output[1]);
+        assertEquals("Joueur 1 utilise Coup d'Épée et inflige 100 dommages", output[1]);
+        assertEquals("Joueur 2 utilise Coup d'Épée et inflige 100 dommages", output[1]);
+        assertEquals("Joueur 1 utilise Coup d'Épée et inflige 100 dommages", output[1]);
+        assertEquals("Joueur 2 utilise Coup d'Épée et inflige 100 dommages", output[1]);
     }
 
     @Test
