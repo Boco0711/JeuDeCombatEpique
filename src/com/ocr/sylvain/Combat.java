157878b383a629b1ec74d8ac2bfb706b7986a137
Boco0711 <s.lep.0711@gmail.com>
2019-09-04 22:33:49 +0200

    Ajout du code de comportement dans la fonction combat

--- src/com/ocr/sylvain/Combat.java
+++ src/com/ocr/sylvain/Combat.java
@@ -1,4 +1,137 @@
 package com.ocr.sylvain;
 
+import java.util.Scanner;
+
 public class Combat {
+    private Character joueur1;
+    private Character joueur2;
+
+    Scanner sc = new Scanner(System.in);
+
+    public Combat(Character joueur1, Character joueur2) {
+        this.joueur1 = joueur1;
+        this.joueur2 = joueur2;
+    }
+
+    /**
+     *  Run fight of the 2 player
+     * @param joueur1 one of the 2 player
+     * @param joueur2 second of the 2 player
+     */
+    public void startCombat(Character joueur1, Character joueur2) {
+        while (joueur1.getVitality() > 0 && joueur2.getVitality() > 0) {
+            fight(joueur1, joueur2);
+            if (joueur1.getVitality() > 0 && joueur2.getVitality() > 0)
+            fight(joueur2, joueur1);
+        }
+
+    }
+
+    /**
+     *  Ask a question about the fight in standart input, get response and call correct action
+     * @param attacker the player that attack
+     * @param defender the player that will be attacked
+     */
+    public void fight(Character attacker, Character defender) {
+        int choiceOfAttack = 0;
+        while (choiceOfAttack < 1 || choiceOfAttack > 2) {
+            System.out.println(attacker.getName() + " (" + attacker.getVitality() + " vitalité) veuillez choisir votre action (1 : " + getBasicAttack(attacker.getWarcraft()) + ", 2 : " + getSpecialAttack(attacker.getWarcraft()) + ")");
+            choiceOfAttack = sc.nextInt();
+            if (choiceOfAttack == 1)
+                basicActionEffect(attacker, defender);
+            else if (choiceOfAttack == 2)
+                specialActionEffect(attacker, defender);
+            else
+                System.out.println("Vous n'avez pas choisi une action valide");
+        }
+
+    }
+
+    /**
+     * Return the name of Basic attack depending on the player's warcraft
+     * @param warcraft player's warcraft
+     * @return the name of the basic attack
+     */
+        String getBasicAttack(String warcraft) {
+        switch (warcraft) {
+            case "Guerrier":
+                return ("Coup d'Épée");
+            case "Rodeur":
+                return ("Tir à l'Arc");
+            case "Mage":
+                return ("Boule de Feu");
+        }
+        return null;
+    }
+
+    /**
+     * Return the name of Special attack depending on the player's warcraft
+     * @param warcraft
+     * @return the name of special attack
+     */
+    String getSpecialAttack(String warcraft) {
+        switch (warcraft) {
+            case "Guerrier":
+                return ("Coup de Rage");
+            case "Rodeur":
+                return ("Concentration");
+            case "Mage":
+                return ("Soin");
+        }
+        return null;
+    }
+
+    /**
+     * Display the effect of a basic attack depending on the attacker's warcraft
+     * @param attacker player that used the basic attack
+     * @param defender player that will be hit by the basic attack
+     */
+    private void basicActionEffect(Character attacker, Character defender) {
+        switch (attacker.getWarcraft()) {
+            case "Guerrier":
+                System.out.println(attacker.getName() + " utilise " + getBasicAttack(attacker.getWarcraft()) + " et inflige " + attacker.getStrenght() + " dommages");
+                defender.receiveDamage(attacker.getStrenght());
+                break;
+            case "Rodeur":
+                System.out.println(attacker.getName() + " utilise " + getBasicAttack(attacker.getWarcraft()) + " et inflige " + attacker.getAgility() + " dommages");
+                defender.receiveDamage(attacker.getAgility());
+                break;
+            case "Mage":
+                System.out.println(attacker.getName() + " utilise " + getBasicAttack(attacker.getWarcraft()) + " et inflige " + attacker.getIntelligence() + " dommages");
+                defender.receiveDamage(attacker.getIntelligence());
+                break;
+        }
+        if (defender.getVitality() == 0)
+            System.out.println(defender.getName() + " est mort");
+    }
+
+    /**
+     * Display the effect of a special attack depending on the attacker's warcraft
+     * @param attacker player that used the special attack
+     * @param defender player that may be hit by special attack depending on attacker's warcraft
+     */
+    private void specialActionEffect(Character attacker, Character defender) {
+        switch (attacker.getWarcraft()) {
+            case "Guerrier":
+                System.out.println(attacker.getName() + " utilise " + getSpecialAttack(attacker.getWarcraft()) + " et inflige " + attacker.getStrenght() * 2 + " dommages");
+                defender.receiveDamage(attacker.getStrenght() * 2);
+                attacker.receiveDamage(attacker.getStrenght() / 2);
+                break;
+            case "Rodeur":
+                System.out.println(attacker.getName() + " utilise " + getSpecialAttack(attacker.getWarcraft()) + " et gagne " + attacker.getLevel() / 2 + " d'agilité");
+                attacker.addAgility(attacker.getLevel() / 2);
+                break;
+            case "Mage":
+                if(attacker.getVitality() <= (attacker.getLevel()*5 - attacker.getIntelligence()*2)) {
+                    System.out.println(attacker.getName() + " utilise " + getSpecialAttack(attacker.getWarcraft()) + " et gagne " + attacker.getIntelligence() * 2 + " en vitalité");
+                    attacker.addHealth(attacker.getIntelligence() * 2);
+                } else {
+                    System.out.println(attacker.getName() + " utilise " + getSpecialAttack(attacker.getWarcraft()) + " et gagne " + (attacker.getLevel()*5 - attacker .getVitality() ) + " en vitalité");
+                    attacker.addHealth(attacker.getIntelligence() * 2);
+                }
+                break;
+        }
+    }
+
+
 }
--- src/com/ocr/sylvain/Main.java
+++ src/com/ocr/sylvain/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Character joueur1 = new Character("Joueur 1");
         Character joueur2 = new Character("Joueur 2");
+        Combat combat = new Combat(joueur1, joueur2);
+        combat.startCombat(joueur1, joueur2);
     }
 }
