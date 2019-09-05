f43aef24783207228f974d292410e8fc55aa6574
Boco0711 <s.lep.0711@gmail.com>
2019-09-05 14:10:42 +0200

    Les test manquais de logique, Je les ai ai changé. J'ai changé la méthode testé, au lieu de tester la grosse methode de combat StartCombat je test maintenant la méthode plus petite : fight ( qui est appelé en boucle dans start combat );

--- test/com/ocr/sylvain/CombatTest.java
+++ test/com/ocr/sylvain/CombatTest.java
@@ -33,25 +33,18 @@ class CombatTest {
 
     @Test
     public void Given_WarriorBasicAttack_When_CombatIsRun_Then_DisplaySentence() {
-        System.setIn(new ByteArrayInputStream("1\n1\n1\n1\n1\n1\n1\n1\n1\n".getBytes()));
+        System.setIn(new ByteArrayInputStream("1\n".getBytes()));
         Combat combat = new Combat(joueur1, joueur2);
-        combat.startCombat(joueur1, joueur2);
+        combat.fight(joueur1, joueur2);
         String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
         assertEquals("Joueur 1 utilise Coup d'Épée et inflige 100 dommages", output[1]);
-        assertEquals("Joueur 2 utilise Tir à l'Arc et inflige 100 dommages", output[3]);
-        assertEquals("Joueur 1 utilise Coup d'Épée et inflige 100 dommages", output[5]);
-        assertEquals("Joueur 2 utilise Tir à l'Arc et inflige 100 dommages", output[7]);
-        assertEquals("Joueur 1 utilise Coup d'Épée et inflige 100 dommages", output[9]);
-        assertEquals("Joueur 2 utilise Tir à l'Arc et inflige 100 dommages", output[11]);
-        assertEquals("Joueur 1 utilise Coup d'Épée et inflige 100 dommages", output[13]);
-        assertEquals("Joueur 2 utilise Tir à l'Arc et inflige 100 dommages", output[15]);
     }
 
     @Test
     public void Given_WarriorSpecialAttack_When_CombatIsRun_Then_DisplaySentence() {
         System.setIn(new ByteArrayInputStream("2\n".getBytes()));
         Combat combat = new Combat(joueur1, joueur2);
-        combat.startCombat(joueur1, joueur2);
+        combat.fight(joueur1, joueur2);
         String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
         assertEquals("Joueur 1 utilise Coup de Rage et inflige 200 dommages", output[1]);
     }
@@ -60,7 +53,7 @@ class CombatTest {
     public void Given_RodeurBasicAttack_When_CombatIsRun_Then_DisplaySentence() {
         System.setIn(new ByteArrayInputStream("1\n".getBytes()));
         Combat combat = new Combat(joueur2, joueur3);
-        combat.startCombat(joueur2, joueur3);
+        combat.fight(joueur2, joueur3);
         String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
         assertEquals("Joueur 2 utilise Tir à l'Arc et inflige 100 dommages", output[1]);
     }
@@ -69,7 +62,7 @@ class CombatTest {
     public void Given_RodeurSpecialAttack_When_CombatIsRun_Then_DisplaySentence() {
         System.setIn(new ByteArrayInputStream("2\n".getBytes()));
         Combat combat = new Combat(joueur2, joueur3);
-        combat.startCombat(joueur2, joueur3);
+        combat.fight(joueur2, joueur3);
         String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
         assertEquals("Joueur 2 utilise Concentration et gagne 50 d'agilité", output[1]);
     }
@@ -78,7 +71,7 @@ class CombatTest {
     public void Given_MageBasicAttack_When_CombatIsRun_Then_DisplaySentence() {
         System.setIn(new ByteArrayInputStream("1\n".getBytes()));
         Combat combat = new Combat(joueur3, joueur2);
-        combat.startCombat(joueur3, joueur2);
+        combat.fight(joueur3, joueur2);
         String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
         assertEquals("Joueur 3 utilise Boule de Feu et inflige 100 dommages", output[1]);
     }
@@ -88,7 +81,7 @@ class CombatTest {
         System.setIn(new ByteArrayInputStream("2\n".getBytes()));
         joueur3.receiveDamage(300);
         Combat combat = new Combat(joueur3, joueur2);
-        combat.startCombat(joueur3, joueur2);
+        combat.fight(joueur3, joueur2);
         String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
         assertEquals("Joueur 3 utilise Soin et gagne 200 en vitalité", output[1]);
     }
@@ -97,7 +90,7 @@ class CombatTest {
     public void Given_MageSpecialAttackAtFullHP_When_CombatIsRun_Then_DisplaySentence() {
         System.setIn(new ByteArrayInputStream("2\n".getBytes()));
         Combat combat = new Combat(joueur3, joueur2);
-        combat.startCombat(joueur3, joueur2);
+        combat.fight(joueur3, joueur2);
         String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
         assertEquals("Joueur 3 utilise Soin et gagne 0 en vitalité", output[1]);
     }
@@ -106,7 +99,7 @@ class CombatTest {
     public void Given_BadValue_When_CombatIsRun_Then_DisplayErrorSentence() {
         System.setIn(new ByteArrayInputStream("4\n2\n".getBytes()));
         Combat combat = new Combat(joueur1, joueur2);
-        combat.startCombat(joueur1, joueur2);
+        combat.fight(joueur1, joueur2);
         String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
         assertEquals("Vous n'avez pas choisi une action valide", output[1]);
         assertEquals("Joueur 1 utilise Coup de Rage et inflige 200 dommages", output[3]);
